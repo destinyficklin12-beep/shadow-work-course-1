b110ea0473b3ca0a974ef3a17e4fc3fd13f6b884
destinyficklin12-beep <destinyficklin12@gmail.com>
2025-12-31 03:50:05 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 // Firebase Configuration
 // REPLACE WITH YOUR ACTUAL CONFIG FROM FIREBASE CONSOLE
-
 const firebaseConfig = {
   apiKey: "AIzaSyDb1UXjiEUJX20EigX00IaKupWnVunYy6I",
   authDomain: "shadow-work-course.firebaseapp.com",
@@ -10,7 +9,6 @@ const firebaseConfig = {
   appId: "1:1023145338538:web:e1c35722de8b7480fe92d3",
   measurementId: "G-QXCDKWYJP6"
 };
-
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
